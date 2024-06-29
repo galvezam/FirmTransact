2ca2572f0229c550982c4f0bd68ac931c15362b1
galvezam <mgalvez644@icloud.com>
2024-06-28 20:15:09 -0500

    spring boot initial project files

---
+++ FirmTransact/src/main/java/com/galvez/FirmTransact/FirmTransactApplication.java
@@ -0,0 +1,13 @@
+package com.galvez.FirmTransact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirmTransactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirmTransactApplication.class, args);
+	}
+
+}
---
+++ FirmTransact/src/test/java/com/galvez/FirmTransact/FirmTransactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galvez.FirmTransact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirmTransactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
